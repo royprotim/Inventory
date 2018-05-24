80bdc804277c8a2f8a70fbb2c64dde2fe44ca6d6
Protim <protimr16@gmail.com>
2018-05-24 20:06:35 +0530

    Moved Executions to Models

--- warehouse/models.py
+++ warehouse/models.py
@@ -8,6 +8,10 @@ class Warehouse(models.Model):
 	location_id = models.AutoField(primary_key=True)
 	city = models.CharField(max_length=50)
 	img_loc = models.CharField(max_length=50,default='null')
+
+	
+
+
 	def __unicode__(self):
 		return self.city
 
@@ -17,6 +21,30 @@ class Items(models.Model):
 	item_name = models.CharField(max_length=50)
 	qty = models.IntegerField()
 	warehouse_location = models.ForeignKey(Warehouse,on_delete=models.CASCADE)
+
+	def add_data(self,add_qty):
+		self.qty = self.qty + int(add_qty)
+		log = Log.objects.create(item_name=self.item_name,qty=int(add_qty),location_to=self.warehouse_location)
+		log.save()
+
+	def move_data(self,add_qty, warehouse_loc_to):
+		self.qty = self.qty-int(add_qty)
+		#print "Moving ",Item_from.qty," ",Item_from.item_name," from ",warehouse_loc_from.city," to ",warehouse_loc_to.city
+		self.save()
+					# Item_to = Items.objects.get(warehouse_location=warehouse_loc.location_id,item_name=item)
+		if Items.objects.filter(warehouse_location=warehouse_loc_to.location_id,item_name=self.item_name).exists():
+			Item_to = Items.objects.get(warehouse_location=warehouse_loc_to.location_id,item_name=self.item_name)
+			
+			Item_to.add_data(add_qty)
+			Item_to.save()
+			log = Log.objects.create(item_name=self.item_name,qty=add_qty,location_from=self.warehouse_location,location_to=warehouse_loc_to)
+			log.save()
+		else:
+			Item_to = Items.objects.create(item_name=self.item_name,qty=add_qty,warehouse_location=warehouse_loc_to)
+			Item_to.save()
+			log = Log.objects.create(item_name=self.item_name,qty=add_qty,location_from=self.warehouse_location,location_to=warehouse_loc_to)
+			log.save()
+
 	def __unicode__(self):
 		return self.item_name
 
--- warehouse/views.py
+++ warehouse/views.py
@@ -13,10 +13,11 @@ def add(request):
 		location,item = request.POST['location'].split("_")
 		warehouse_loc = Warehouse.objects.get(city=location)
 		Item = Items.objects.get(warehouse_location=warehouse_loc.location_id,item_name=item)
-		Item.qty = Item.qty+int(add_qty)
+		print add_qty
+		Item.add_data(add_qty)
 		Item.save()
-		log = Log.objects.create(item_name=item,qty=add_qty,location_to=warehouse_loc)
-		log.save()
+		# log = Log.objects.create(item_name=item,qty=add_qty,location_to=warehouse_loc)
+		# log.save()
 	
 	return index(request)
 
@@ -29,63 +30,13 @@ def move(request):
 		warehouse_loc_from = Warehouse.objects.get(city=location)
 		Item_from = Items.objects.get(warehouse_location=warehouse_loc_from.location_id,item_name=item)
 		warehouse_loc_to = Warehouse.objects.get(city=to_location)
-		print "Moving ",Item_from.qty," ",Item_from.item_name," from ",warehouse_loc_from.city," to ",warehouse_loc_to.city
-		Item_from.qty = Item_from.qty-int(add_qty)
-		print "Moving ",Item_from.qty," ",Item_from.item_name," from ",warehouse_loc_from.city," to ",warehouse_loc_to.city
-		Item_from.save()
-					# Item_to = Items.objects.get(warehouse_location=warehouse_loc.location_id,item_name=item)
-		if Items.objects.filter(warehouse_location=warehouse_loc_to.location_id,item_name=item).exists():
-			Item_to = Items.objects.get(warehouse_location=warehouse_loc_to.location_id,item_name=item)
-			
-			Item_to.qty = Item_to.qty+int(add_qty)
-			Item_to.save()
-			log = Log.objects.create(item_name=item,qty=add_qty,location_from=warehouse_loc_from,location_to=warehouse_loc_to)
-			log.save()
-		else:
-			Item_to = Items.objects.create(item_name=item,qty=add_qty,warehouse_location=warehouse_loc_to)
-			Item_to.save()
-			log = Log.objects.create(item_name=item,qty=add_qty,location_from=warehouse_loc_from,location_to=warehouse_loc_to)
-			log.save()
+		#print "Moving ",Item_from.qty," ",Item_from.item_name," from ",warehouse_loc_from.city," to ",warehouse_loc_to.city
+		Item_from.move_data(add_qty,warehouse_loc_to)
 		
 	return index(request)
 
 
 def index(request):
-	# if request.method=='POST':
-	# 	if 'input_qty2' not in request.POST:
-	# 		add_qty = request.POST['input_qty']
-	# 		location,item = request.POST['location'].split("_")
-	# 		warehouse_loc = Warehouse.objects.get(city=location)
-	# 		Item = Items.objects.get(warehouse_location=warehouse_loc.location_id,item_name=item)
-	# 		Item.qty = Item.qty+int(add_qty)
-	# 		Item.save()
-	# 		log = Log.objects.create(item_name=item,qty=add_qty,location_to=warehouse_loc)
-	# 		log.save()
-	# 	else:
-	# 		add_qty = request.POST['input_qty2']
-	# 		location,item = request.POST['location'].split("_")
-	# 		to_location = request.POST['to_location']
-	# 		warehouse_loc_from = Warehouse.objects.get(city=location)
-
-	# 		Item_from = Items.objects.get(warehouse_location=warehouse_loc_from.location_id,item_name=item)
-	# 		warehouse_loc_to = Warehouse.objects.get(city=to_location)
-	# 		print "Moving ",Item_from.qty," ",Item_from.item_name," from ",warehouse_loc_from.city," to ",warehouse_loc_to.city
-	# 		Item_from.qty = Item_from.qty-int(add_qty)
-	# 		print "Moving ",Item_from.qty," ",Item_from.item_name," from ",warehouse_loc_from.city," to ",warehouse_loc_to.city
-	# 		Item_from.save()
-	# 					# Item_to = Items.objects.get(warehouse_location=warehouse_loc.location_id,item_name=item)
-	# 		if Items.objects.filter(warehouse_location=warehouse_loc_to.location_id,item_name=item).exists():
-	# 			Item_to = Items.objects.get(warehouse_location=warehouse_loc_to.location_id,item_name=item)
-				
-	# 			Item_to.qty = Item_to.qty+int(add_qty)
-	# 			Item_to.save()
-	# 			log = Log.objects.create(item_name=item,qty=add_qty,location_from=warehouse_loc_from,location_to=warehouse_loc_to)
-	# 			log.save()
-	# 		else:
-	# 			Item_to = Items.objects.create(item_name=item,qty=add_qty,warehouse_location=warehouse_loc_to)
-	# 			Item_to.save()
-	# 			log = Log.objects.create(item_name=item,qty=add_qty,location_from=warehouse_loc_from,location_to=warehouse_loc_to)
-	# 			log.save()
 	all_location = Warehouse.objects.all()
 	loc_imgs = {}
 	response = {}
